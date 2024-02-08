0ba0141baf5ffcb090d3ce98e7e63d1fb1816d7b
Artur Gauzer <artur.gauzer@pm.me>
2024-02-08 20:21:43 +0600

    Add pagination to `GET /articles`

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -12,7 +12,7 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        return ArticleResource::collection(Article::all());
+        return ArticleResource::collection(Article::paginate());
     }
 
     /**

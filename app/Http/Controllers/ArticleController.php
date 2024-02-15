82427463432e162d0945af81670c5c03e374da94
Artur Gauzer <artur.gauzer@pm.me>
2024-02-15 22:25:05 +0600

    Let authors delete their own articles

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -8,6 +8,14 @@ use App\Models\Article;
 class ArticleController extends Controller
 {
     /**
+     * Create the controller instance.
+     */
+    public function __construct()
+    {
+        $this->authorizeResource(Article::class, 'article');
+    }
+
+    /**
      * Display a listing of the resource.
      */
     public function index()
@@ -25,5 +33,15 @@ class ArticleController extends Controller
         $article->increment('views');
 
         return new ArticleResource($article);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Article $article)
+    {
+        $article->delete();
+
+        return response(status: 204);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,13 @@ use Illuminate\Support\Facades\Route;
 Route::apiResource('articles', ArticleController::class)
     ->only(['index', 'show']);
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::apiResource('articles', ArticleController::class)
+        ->only(['destroy']);
 });
 
 

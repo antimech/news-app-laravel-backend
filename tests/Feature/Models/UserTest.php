55798d0567ac09ee60914ebe181e84e46bb82511
Artur Gauzer <artur.gauzer@pm.me>
2024-02-16 00:53:38 +0600

    Add the user model test

---
+++ tests/Feature/Models/UserTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use App\Models\Article;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    public function test_a_user_can_have_articles()
+    {
+        $user = User::factory()->create();
+
+        Article::factory()
+            ->count(3)
+            ->for($user, 'author')
+            ->create();
+
+        $this->assertCount(3, $user->articles);
+        $this->assertInstanceOf(Article::class, $user->articles->first());
+    }
+}

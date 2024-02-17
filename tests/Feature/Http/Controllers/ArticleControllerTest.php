5760f932ca48f698789e11ce27ee70e88748527c
Artur Gauzer <artur.gauzer@pm.me>
2024-02-17 13:49:53 +0600

    Skip authorization tests in the ArticleControllerTest

--- tests/Feature/Http/Controllers/ArticleControllerTest.php
+++ tests/Feature/Http/Controllers/ArticleControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Http\Controllers;
 use App\Models\Article;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Gate;
 use Tests\TestCase;
 
 class ArticleControllerTest extends TestCase
@@ -14,6 +15,12 @@ class ArticleControllerTest extends TestCase
      */
     public function test_index(): void
     {
+        // Assuming authorization policy tests cover this scenario, so we don't test it here.
+        // Override the "authorize" method of the Gate facade with a no-op function
+        Gate::shouldReceive('authorize')->andReturn(true);
+
+        $articles = Article::factory()->count(3)->create();
+
         $response = $this->get('/api/articles');
 
         $response->assertOk();
@@ -21,6 +28,10 @@ class ArticleControllerTest extends TestCase
 
     public function test_show(): void
     {
+        // Assuming authorization policy tests cover this scenario, so we don't test it here.
+        // Override the "authorize" method of the Gate facade with a no-op function
+        Gate::shouldReceive('authorize')->andReturn(true);
+
         $article = Article::factory()->create();
 
         $response = $this->get('/api/articles/' . $article->id);
@@ -30,6 +41,10 @@ class ArticleControllerTest extends TestCase
 
     public function test_destroy(): void
     {
+        // Assuming authorization policy tests cover this scenario, so we don't test it here.
+        // Override the "authorize" method of the Gate facade with a no-op function
+        Gate::shouldReceive('authorize')->andReturn(true);
+
         $article = Article::factory()->create();
 
         $this->actingAs($article->author);
